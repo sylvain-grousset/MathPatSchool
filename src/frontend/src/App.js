9dfcd2a8d3515fb7f953ae52d17c09519fab43c2
martin-watkinss <68780393+martin-watkinss@users.noreply.github.com>
2020-10-09 17:28:45 +0200

    creation page login

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    username: '',
+    password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Bonjour ' + this.state.username + this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+
+      <form onSubmit={this.handleSubmit} class='App-header'>
+      <h1>Bienvenue</h1>
+        <label>
+            Login : <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
+        </label>
+        <label>
+            Mot de passe : <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+    );
+  }
 }
 
 export default App;
