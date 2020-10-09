550bbeecd83d6ff0c55d6a578aadd6305e135cb8
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2020-10-09 17:33:39 +0200

    CRUD operation

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -11,17 +11,18 @@ const Cours = require('./models/cours');
 
 var url = 'mongodb://localhost:27017/test';
 
+//connection Ã  la bDD mongoDB
 mongoose.connect(url,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .then(() => console.log('Connexion ï¿½ MongoDB rï¿½ussie !'))
+    .catch(() => console.log('Connexion ï¿½ MongoDB ï¿½chouï¿½e !'));
 
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
@@ -30,6 +31,7 @@ app.get('/', function (req, res) {
 });
    
 
+//Ajout d'un cours
 app.post('/addCours', function(req,res) {
   const cours = new Cours({
     nom_cours: req.body.nom_cours,
@@ -37,22 +39,54 @@ app.post('/addCours', function(req,res) {
     heure_debut: req.body.heure_debut,
     heure_fin: req.body.heure_fin,
     description: req.body.description,
+    contenu: req.body.contenu,
+    
   });
   cours.save().then(val => {
     res.json({ msg: "Cours Added Successfully", val: val })
   })
 })
-
-app.post('/addParcours', function(req, res){
-    const parcours = new Parcours({
-        titre: req.body.titre,
-        description: req.body.description,
-        cours: req.body.cours,
-    });
-    parcours.save().then(val => {
-        res.json({msg: "Parcours addedd Successfuly", val: val})
-    })
+	
+// delete a employee by id
+app.delete('/Delete/Cours/:nom_cours', function(req, res) {
+	console.log(req.params.nom_cours);
+	let nom_cours = req.params.nom_cours;
+	Employee.remove({
+		nom_cours : nom_cours
+	}, function(err) {
+		if (err)
+			res.send(err);
+		else
+			res.send('Successfully! Employee has been Deleted.');	
+	});
 });
+
+
+app.get('/Cours/:date', function(req, res) {
+	let dateEnter = req.params.date;
+	Cours.find({
+    date: dateEnter
+  }) function(err){
+    if(err)
+      res.send(err)
+
+    res.json()
+  }
+ 
+});
+
+
+//get all employee data from db
+app.get('/Cours', function(req, res) {
+	// use mongoose to get all todos in the database
+	Cours.find(function(err, cours) {
+		// if there is an error retrieving, send the error otherwise send data
+		if (err)
+			res.send(err)
+		res.json(cours); // return all employees in JSON format
+	});
+});
+
 
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
--- src/Backend/models/Cours.js
+++ src/Backend/models/Cours.js
@@ -3,30 +3,25 @@ const mongoose = require('mongoose');
 const coursSchema = mongoose.Schema({
 	nom_cours: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	date: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	heure_debut: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	heure_fin: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	description: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
-
+	contenu: {type: Array}
 
 });
 
