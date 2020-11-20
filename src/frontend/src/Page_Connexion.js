b430658a6ad886a312444f6420d6a9f3dd5709b0
Max Wacquier <70647114+max-wacquier@users.noreply.github.com>
2020-11-20 17:42:37 +0100

    new

---
+++ src/frontend/src/Page_Connexion.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export class Page_Connexion extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            username: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+}
+
+handleSubmit(event) {
+    alert("Le nom d'utilisateur a été soumis : " + this.state.username + " ainsi que le mot de passe : " + this.state.password);
+    event.preventDefault();
+}
+    
+render() {
+    return (
+        <form onSubmit={this.handleSubmit} class='App-header'>
+            <h1>Bienvenue sur notre page de connexion !</h1><br />
+            <center><label>Nom d'utilisateur : </label><input type="text" name="username" value={this.state.username} onChange={this.handleChange} /></center><br />
+            <center><label>Mot de passe : </label><input type="password" name="password" value={this.state.password} onChange={this.handleChange} /></center><br />
+            <center><input type="submit" value="Envoyer"></input></center><br />
+            <center><a href="https://www.w3schools.com/">Mot de passe oublié ?</a></center><br />
+            <center><a href="https://www.w3schools.com/">S'enregistrer ?</a></center><br />
+            <input type="button" value="Annuler"></input>
+        </form>
+    );
+
+}
+
+}
+
+export default Page_Connexion;
