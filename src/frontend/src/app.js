0380e888d2330470a070d348834c1180509c9b1b
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2020-12-04 14:12:48 +0100

    App.js

--- src/frontend/src/app.js
+++ src/frontend/src/app.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './App.css';
-import Page_Connexion from './Page_Connexion.js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+
+        this.state = {
+            username: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        window.location.replace("http://localhost:3000/login?utilisateur="+this.state.username+"&mdp="+this.state.password);
+        event.preventDefault();
+    }
 
     render() {
         return (
-            <Page_Connexion />
+            <form onSubmit={this.handleSubmit} class='App-header'>
+                <h1>Bienvenue sur notre page de connexion !</h1><br/>
+                <center><label>Nom d'utilisateur : </label><input type="text" name="username" value={this.state.username} onChange={this.handleChange}/></center><br/>
+                <center><label>Mot de passe : </label><input type="password" name="password" value={this.state.password} onChange={this.handleChange}/></center><br/>
+                <center><input type="submit" value="Envoyer"></input></center><br/>
+                <center><a href="https://www.w3schools.com/">Mot de passe oublié ?</a></center><br/>
+                <center><a href="https://www.w3schools.com/">S'enregistrer ?</a></center><br/>
+            </form>
         );
     }
 }
